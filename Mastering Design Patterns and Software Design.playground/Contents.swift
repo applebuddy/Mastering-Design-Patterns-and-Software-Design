e9b0763e1105e5d4f57cc0c530f779e28044ae62
Applebuddy <applebuddy@nate.com>
2025-06-26 23:42:09 +0900

    [Study, Section 3] Singleton Design Pattern

--- Mastering Design Patterns and Software Design.playground/Contents.swift
+++ Mastering Design Patterns and Software Design.playground/Contents.swift
@@ -1,5 +1,72 @@
 import UIKit
 
+// MARK: Singleton Design Pattern
+
+final class ColorPicker {
+    var colors: [UIColor] = [.gray, .green, .red]
+    nonisolated(unsafe) static let shared: ColorPicker = .init()
+
+    // 싱글톤 객체는 다른 인스턴스가 생성되지 않도록 생성자를 private 하게 지정한다.
+    private init() {
+
+    }
+
+    func getRandomColor() -> UIColor? {
+        return colors.randomElement()
+    }
+}
+
+class A {
+    init() {
+        // 싱글톤 객체를 접근하는 유일한 방법
+        if let color = ColorPicker.shared.getRandomColor() {
+            print(color)
+        }
+    }
+
+    func addColor(color: UIColor) {
+        ColorPicker.shared.colors.append(color)
+    }
+}
+
+class B {
+    init() {
+        if let color = ColorPicker.shared.getRandomColor() {
+            print(color)
+        }
+    }
+
+    func displayColors() {
+        print(ColorPicker.shared.colors)
+    }
+}
+
+class C {
+    init() {
+        if let color = ColorPicker.shared.getRandomColor() {
+            print(color)
+        }
+    }
+
+    func displayColors() {
+        print(ColorPicker.shared.colors)
+    }
+}
+
+let a: A = A()
+let b: B = B()
+let c: C = C()
+
+print("Testing Data Members")
+
+// global 하게 A에서 color를 추가하고, 추가한 color를 포함해서 다른 B 인스턴스에서 출력할 수 있다.
+a.addColor(color: .yellow)
+b.displayColors()
+
+/*
+
+// MARK: Prototype Design Pattern
+
 class Person {
     var name: String
     var age: Int
@@ -25,3 +92,4 @@ person2.name = "Kim"
 person2.age = 30
 
 debugPrint("person name : \(person.name), person2 name : \(person2.name)")
+*/
--- README.md
+++ README.md
@@ -9,7 +9,7 @@ Mastering Design Patterns and Software Design
   - structural : proxy pattern
   - behavioral : observer pattern, responsibility pattern
 
-### Prototype Design Pattern
+## Section 2: Prototype Design Pattern
 
 - Prototype Design Pattern은 Creational Design Pattern에 속합니다.
 - 기존 객체의 멤버를 활용해서 새로운 객체를 생성 가능
@@ -24,3 +24,19 @@ Mastering Design Patterns and Software Design
   - 복사할때 복사되는 멤버가 class 인 경우, 메모리를 공유하고, clone 수정 시 원본에 영향을 줄 수 있으므로 주의
     - Shallow Copy : class는 인스턴스 복사 시 메모리를 공유
     - Deep Copy : struct를 사용하거나 class 이더라도 copy 메서드로 새로운 인스턴스를 생성하는 방식으로 멤버를 전달하면 메모리 공유없이 clone 생성 가능 
+
+
+
+## Section 3: Singleton Design Pattern
+
+- Creational Design Pattern
+- class가 오직 단 하나만 생성되도록 제한
+- 시스템 내에서 필요한 영역 내에서 global하게 단일 인스턴스를 사용 가능
+- Database Helper, Network Manager 등에서 활용 가능
+- global하게 사용될 필요가 없거나, 시스템 내에서 단일 인스턴스로 대표할 필요가 없으면 불필요한 Design Pattern이 될 수 있음
+- Data races 문제 등 방지를 위한 동시성 보호 고려가 필요함
+- iOS 기본 SDK에 싱글톤 객체들이 있습니다.
+  - ex) UIApplication, UserDefaults, NotificationCenter ...
+- final 키워드
+  - final 키워드를 사용하면, 해당 클래스는 서브클래스가 될 수 없다. 컴파일러가 해당 타입을 추론하기 쉬워짐
+  - static dispatch 방식으로 사용 -> 컴파일러가 해당 클래스의 타입을 쉽게 추론할 수 있게하여 응용프로그램 성능 향상
